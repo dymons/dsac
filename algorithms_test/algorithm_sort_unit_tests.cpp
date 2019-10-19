1015f99ad0a8b08f667b95e6097b631d5ea7da32
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2019-10-19 23:11:39 +0300

    Add tests for algorithms sort

--- algorithms_test/algorithm_sort_unit_tests.cpp
+++ algorithms_test/algorithm_sort_unit_tests.cpp
@@ -62,6 +62,32 @@ TEST_F(AlgorithmsSortTests, selection_sort_full)
   EXPECT_TRUE(std::is_sorted(full_reverse_sequence.begin(), full_reverse_sequence.end()));
 }
 
+TEST_F(AlgorithmsSortTests, selection_sort_forward)
+{
+  std::vector<int> mt19937_sequence { };
+  std::random_device rd;
+  std::mt19937 generator(rd());
+  std::uniform_int_distribution<> distribution(0, 9999);
+  std::generate_n(std::inserter(mt19937_sequence, mt19937_sequence.end()), 10000, [&](){return distribution(generator);});
+
+  algorithms::selection_sort(mt19937_sequence.begin(), mt19937_sequence.end());
+  EXPECT_TRUE(std::is_sorted(mt19937_sequence.begin(), mt19937_sequence.end()));
+  EXPECT_TRUE(!std::is_sorted(mt19937_sequence.rbegin(), mt19937_sequence.rend()));
+}
+
+TEST_F(AlgorithmsSortTests, selection_sort_backward)
+{
+  std::vector<int> mt19937_sequence { };
+  std::random_device rd;
+  std::mt19937 generator(rd());
+  std::uniform_int_distribution<> distribution(0, 9999);
+  std::generate_n(std::inserter(mt19937_sequence, mt19937_sequence.end()), 10000, [&](){return distribution(generator);});
+
+  algorithms::selection_sort(mt19937_sequence.rbegin(), mt19937_sequence.rend());
+  EXPECT_TRUE(!std::is_sorted(mt19937_sequence.begin(), mt19937_sequence.end()));
+  EXPECT_TRUE(std::is_sorted(mt19937_sequence.rbegin(), mt19937_sequence.rend()));
+}
+
 TEST_F(AlgorithmsSortTests, merge_sort_full)
 {
   std::vector<int> full_reverse_sequence { };
@@ -73,4 +99,30 @@ TEST_F(AlgorithmsSortTests, merge_sort_full)
 
   algorithms::merge_sort(full_reverse_sequence.begin(), full_reverse_sequence.end());
   EXPECT_TRUE(std::is_sorted(full_reverse_sequence.begin(), full_reverse_sequence.end()));
+}
+
+TEST_F(AlgorithmsSortTests, merge_sort_forward)
+{
+  std::vector<int> mt19937_sequence { };
+  std::random_device rd;
+  std::mt19937 generator(rd());
+  std::uniform_int_distribution<> distribution(0, 9999);
+  std::generate_n(std::inserter(mt19937_sequence, mt19937_sequence.end()), 10000, [&](){return distribution(generator);});
+
+  algorithms::merge_sort(mt19937_sequence.begin(), mt19937_sequence.end());
+  EXPECT_TRUE(std::is_sorted(mt19937_sequence.begin(), mt19937_sequence.end()));
+  EXPECT_TRUE(!std::is_sorted(mt19937_sequence.rbegin(), mt19937_sequence.rend()));
+}
+
+TEST_F(AlgorithmsSortTests, merge_sort_backward)
+{
+  std::vector<int> mt19937_sequence { };
+  std::random_device rd;
+  std::mt19937 generator(rd());
+  std::uniform_int_distribution<> distribution(0, 9999);
+  std::generate_n(std::inserter(mt19937_sequence, mt19937_sequence.end()), 10000, [&](){return distribution(generator);});
+
+  algorithms::merge_sort(mt19937_sequence.rbegin(), mt19937_sequence.rend());
+  EXPECT_TRUE(!std::is_sorted(mt19937_sequence.begin(), mt19937_sequence.end()));
+  EXPECT_TRUE(std::is_sorted(mt19937_sequence.rbegin(), mt19937_sequence.rend()));
 }
