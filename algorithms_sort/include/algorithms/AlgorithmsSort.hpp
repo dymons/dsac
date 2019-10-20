f4074de58317c84c643ea09087a0123c8d38e373
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2019-10-20 12:51:12 +0300

    Update function merge sort

--- algorithms_sort/include/algorithms/AlgorithmsSort.hpp
+++ algorithms_sort/include/algorithms/AlgorithmsSort.hpp
@@ -30,18 +30,20 @@ namespace algorithms {
   template<class Iterator>
     void merge_sort(Iterator t_begin, Iterator t_end)
     {
-      if(t_begin != t_end) {
-        if(t_begin == std::prev(t_end)) {
-          return;
-        }
+      // Recursion ends when the sequence becomes 1
+      if(std::distance(t_begin, t_end) < 2) {
+        return;
       }
 
-      auto merge = [](Iterator first, Iterator middle, Iterator finish) {
-        const std::deque<std::remove_reference_t<decltype(*first)>> left {first, middle}, right {middle, finish};
+      // Combining two sorted sequences
+      auto merge = [](Iterator begin, Iterator middle, Iterator end) {
+        // Supposed, subarrays are sorted
+        const std::deque<std::remove_reference_t<decltype(*begin)>> left {begin, middle}, right {middle, end};
 
         auto left_it = left.begin(), right_it = right.begin();
 
-        for(Iterator itr = first; itr != finish; ++itr) {
+        // Merge two subarrays into one sorted
+        for(Iterator itr = begin; itr != end; ++itr) {
           if(left_it == left.end()) {
             *itr = *right_it++;
           } else if(right_it == right.end()) {
@@ -54,12 +56,10 @@ namespace algorithms {
         }
       };
 
-      if(t_begin != t_end) {
-        Iterator middle = t_begin + static_cast<std::size_t>((std::distance(t_begin, t_end) / 2));
-        merge_sort(t_begin, middle);
-        merge_sort(middle, t_end);
-        merge(t_begin, middle, t_end);
-      }
+      Iterator middle = t_begin + static_cast<std::size_t>((std::distance(t_begin, t_end) / 2));
+      merge_sort(t_begin, middle);
+      merge_sort(middle, t_end);
+      merge(t_begin, middle, t_end);
     }
 } // namespace algorithms
 #endif // ALGORITHMS_ALGORITHMS_SORT_HPP_
