1d714e87b880745b6537fafa34725ed4295d05bb
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2021-10-16 21:42:57 +0300

    1. Добавил ссылку на Shaker Sort в README.md 2. Дол более говорящее имя переменной

--- algorithms/sorts/shaker_sort.hpp
+++ algorithms/sorts/shaker_sort.hpp
@@ -30,23 +30,23 @@ template <typename T>
 void ShakerSort(std::vector<T>& arr) {
   int left = 0;
   int right = (int)arr.size() - 1;
-  int control = (int)arr.size() - 1;
+  int latest_swap = (int)arr.size() - 1;
   do {
     for (int i = left; i < right; ++i) {
       if (arr[i] > arr[i + 1]) {
         std::swap(arr[i], arr[i + 1]);
-        control = i;
+        latest_swap = i;
       }
     }
-    right = control;
+    right = latest_swap;
 
     for (int i = right; i > left; --i) {
       if (arr[i - 1] > arr[i]) {
         std::swap(arr[i - 1], arr[i]);
-        control = i;
+        latest_swap = i;
       }
     }
-    left = control;
+    left = latest_swap;
   } while (left < right);
 }
 }  // namespace algo::sort::v2
