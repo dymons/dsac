36c0b243961c104f87c36564e1df7caa0950e421
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2021-10-17 19:44:05 +0300

    Сделал более удобный способ создания графа для тестирования

--- structures/tests/graph_test.cpp
+++ structures/tests/graph_test.cpp
@@ -1,14 +1,26 @@
 #include <catch2/catch.hpp>
+#include <utility>
 
 #include <graph.hpp>
 
 TEST_CASE("Проверка корректности работы неориентированного графа",
           "[undirected_graph]") {
-  algo::graph::UndirectedGraph<int> graph(4);
-  graph.Add({0}, {1});
-  graph.Add({0}, {2});
-  graph.Add({1}, {3});
-  graph.Add({3}, {2});
+  using Edge = std::pair<int, int>;
+  using Edges = std::vector<Edge>;
+  // clang-format off
+  const Edges edges {
+    {0, 1},
+    {0, 2},
+    {1, 3},
+    {3, 2},
+  };
+  // clang-format on
+
+  algo::graph::UndirectedGraph<int> graph(edges.size());
+  for (const Edge& edge : edges) {
+    graph.Add({edge.first}, {edge.second});
+  }
+
   REQUIRE(graph.GetSuccessors({0}).size() == 2);
   REQUIRE(graph.GetSuccessors({1}).size() == 1);
   REQUIRE(graph.GetSuccessors({2}).empty());
