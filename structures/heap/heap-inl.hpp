e71db637be2828a899cd9f7bafe7ad7e342be12d
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2021-11-05 17:55:50 +0300

    Рефакторинг кода

---
+++ structures/heap/heap-inl.hpp
@@ -0,0 +1,107 @@
+#ifndef STRUCTURES_HEAP_H_
+#error This file may only be included from heap.hpp
+#endif
+
+namespace algo::heap {
+template <typename T, template <typename> class Compare>
+int Heap<T, Compare>::GetIndexLeftChild(int index) noexcept {
+  return 2 * index + 1;
+}
+
+template <typename T, template <typename> class Compare>
+int Heap<T, Compare>::GetIndexRightChild(int index) noexcept {
+  return 2 * index + 2;
+}
+
+template <typename T, template <typename> class Compare>
+int Heap<T, Compare>::GetIndexParent(int index) noexcept {
+  return (index - 1) / 2;
+}
+
+template <typename T, template <typename> class Compare>
+bool Heap<T, Compare>::ExistLeftChild(int index) noexcept {
+  return GetIndexLeftChild(index) < storage_.size();
+}
+
+template <typename T, template <typename> class Compare>
+bool Heap<T, Compare>::ExistRightChild(int index) noexcept {
+  return GetIndexRightChild(index) < storage_.size();
+}
+
+template <typename T, template <typename> class Compare>
+bool Heap<T, Compare>::ExistParent(int index) noexcept {
+  return GetIndexParent(index) >= 0;
+}
+
+template <typename T, template <typename> class Compare>
+T Heap<T, Compare>::GetLeftChild(int index) noexcept {
+  return storage_[GetIndexLeftChild(index)];
+}
+
+template <typename T, template <typename> class Compare>
+T Heap<T, Compare>::GetRightChild(int index) noexcept {
+  return storage_[GetIndexRightChild(index)];
+}
+
+template <typename T, template <typename> class Compare>
+T Heap<T, Compare>::GetParent(int index) noexcept {
+  return storage_[GetIndexParent(index)];
+}
+
+template <typename T, template <typename> class Compare>
+void Heap<T, Compare>::HeapifyDown() {
+  std::size_t index = 0;
+  while (ExistLeftChild(index)) {
+    std::size_t smaller_child_index = GetIndexLeftChild(index);
+    if (ExistRightChild(index) && comp_(GetRightChild(index), GetLeftChild(index))) {
+      smaller_child_index = GetIndexRightChild(index);
+    }
+
+    if (comp_(storage_[index], storage_[smaller_child_index])) {
+      break;
+    }
+
+    std::swap(storage_[index], storage_[smaller_child_index]);
+    index = smaller_child_index;
+  }
+}
+
+template <typename T, template <typename> class Compare>
+void Heap<T, Compare>::HeapifyUp() {
+  std::size_t last_index = storage_.size() - 1;
+  while (ExistParent(last_index) && comp_(storage_[last_index], GetParent(last_index))) {
+    std::size_t const parent_index = GetIndexParent(last_index);
+    std::swap(storage_[parent_index], storage_[last_index]);
+    last_index = parent_index;
+  }
+}
+
+template <typename T, template <typename> class Compare>
+T Heap<T, Compare>::Peek() const {
+  if (storage_.empty()) {
+    throw std::out_of_range{""};
+  }
+
+  return storage_.front();
+}
+
+template <typename T, template <typename> class Compare>
+T Heap<T, Compare>::Pool() {
+  const T min_value = Peek();
+  storage_.front() = storage_.back();
+  storage_.pop_back();
+  HeapifyDown();
+  return min_value;
+}
+
+template <typename T, template <typename> class Compare>
+bool Heap<T, Compare>::IsEmpty() const noexcept {
+  return storage_.empty();
+}
+
+template <typename T, template <typename> class Compare>
+void Heap<T, Compare>::Insert(T key) {
+  storage_.push_back(key);
+  HeapifyUp();
+}
+}  // namespace algo::heap
--- structures/heap/heap.hpp
+++ structures/heap/heap.hpp
@@ -4,110 +4,39 @@
 #include <stdexcept>
 
 namespace algo::heap {
-template <typename T, typename Compare>
+template <typename T, template <typename> class Compare>
 class Heap final {
+  [[gnu::always_inline]] inline int GetIndexLeftChild(int index) noexcept;
+  [[gnu::always_inline]] inline int GetIndexRightChild(int index) noexcept;
+  [[gnu::always_inline]] inline int GetIndexParent(int index) noexcept;
+  [[gnu::always_inline]] inline bool ExistLeftChild(int index) noexcept;
+  [[gnu::always_inline]] inline bool ExistRightChild(int index) noexcept;
+  [[gnu::always_inline]] inline bool ExistParent(int index) noexcept;
+  [[gnu::always_inline]] inline T GetLeftChild(int index) noexcept;
+  [[gnu::always_inline]] inline T GetRightChild(int index) noexcept;
+  [[gnu::always_inline]] inline T GetParent(int index) noexcept;
+
+  void HeapifyDown();
+  void HeapifyUp();
+
   std::vector<T> storage_;
-
-  [[gnu::always_inline]] int GetIndexLeftChild(int index) noexcept {
-    return 2 * index + 1;
-  }
-
-  [[gnu::always_inline]] int GetIndexRightChild(int index) noexcept {
-    return 2 * index + 2;
-  }
-
-  [[gnu::always_inline]] int GetIndexParent(int index) noexcept {
-    return (index - 1) / 2;
-  }
-
-  [[gnu::always_inline]] bool ExistLeftChild(int index) noexcept {
-    return GetIndexLeftChild(index) < storage_.size();
-  }
-
-  [[gnu::always_inline]] bool ExistRightChild(int index) noexcept {
-    return GetIndexRightChild(index) < storage_.size();
-  }
-
-  [[gnu::always_inline]] bool ExistParent(int index) noexcept {
-    return GetIndexParent(index) >= 0;
-  }
-
-  [[gnu::always_inline]] T GetLeftChild(std::size_t index) noexcept {
-    return storage_[GetIndexLeftChild(index)];
-  }
-
-  [[gnu::always_inline]] T GetRightChild(std::size_t index) noexcept {
-    return storage_[GetIndexRightChild(index)];
-  }
-
-  [[gnu::always_inline]] T GetParent(std::size_t index) noexcept {
-    return storage_[GetIndexParent(index)];
-  }
-
-  void SwapElementsByIndex(std::size_t left, std::size_t right) {
-    std::swap(storage_[left], storage_[right]);
-  }
-
-  void HeapifyDown() {
-    std::size_t index = 0;
-    while (ExistLeftChild(index)) {
-      std::size_t smaller_child_index = GetIndexLeftChild(index);
-      if (ExistRightChild(index) && Compare{}(GetRightChild(index), GetLeftChild(index))) {
-        smaller_child_index = GetIndexRightChild(index);
-      }
-
-      if (Compare{}(storage_[index], storage_[smaller_child_index])) {
-        break;
-      }
-
-      SwapElementsByIndex(index, smaller_child_index);
-      index = smaller_child_index;
-    }
-  }
-
-  void HeapifyUp() {
-    std::size_t last_index = storage_.size() - 1;
-    while (ExistParent(last_index) && Compare{}(storage_[last_index], GetParent(last_index))) {
-      std::size_t const parent_index = GetIndexParent(last_index);
-      SwapElementsByIndex(parent_index, last_index);
-      last_index = parent_index;
-    }
-  }
+  Compare<T> comp_;
 
  public:
-  T Peek() const {
-    if (storage_.empty()) {
-      throw std::out_of_range{""};
-    }
+  T Peek() const;
+  [[nodiscard]] bool IsEmpty() const noexcept;
 
-    return storage_.front();
-  }
-
-  T Pool() {
-    if (storage_.empty()) {
-      throw std::out_of_range{""};
-    }
-
-    const T min_value = storage_.front();
-    storage_.front() = storage_.back();
-    storage_.pop_back();
-    HeapifyDown();
-    return min_value;
-  }
-
-  [[nodiscard]] bool IsEmpty() const noexcept {
-    return storage_.empty();
-  }
-
-  void Insert(T key) {
-    storage_.push_back(key);
-    HeapifyUp();
-  }
+  T Pool();
+  void Insert(T key);
 };
 
-template<typename T>
-using MinHeap = Heap<T, std::less<T>>;
+template <typename T>
+using MinHeap = Heap<T, std::less>;
 
-template<typename T>
-using MaxHeap = Heap<T, std::greater<T>>;
+template <typename T>
+using MaxHeap = Heap<T, std::greater>;
 }  // namespace algo::heap
+
+#define STRUCTURES_HEAP_H_
+#include <structures/heap/heap-inl.hpp>
+#undef STRUCTURES_HEAP_H_
