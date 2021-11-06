fc61dac16bf27e9736d38da1ce788e18a7fefcc5
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2021-11-06 15:11:18 +0300

    Добавил простую реализацию SharedPtr

---
+++ cpp/pointers/shared_ptr.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+namespace algo::pointers {
+template <typename T>
+class SharedPtr final {
+ private:
+  T* data_{nullptr};
+  std::size_t* counter_{nullptr};
+
+  void Destroy() {
+    if (counter_ != nullptr) {
+      --*counter_;
+      if (*counter_ == 0) {
+        delete data_;
+        delete counter_;
+      }
+    }
+  }
+
+ public:
+  SharedPtr() noexcept = default;
+  explicit SharedPtr(T* data)
+      : data_(data), counter_(data != nullptr ? new std::size_t{1u} : nullptr) {
+  }
+
+  SharedPtr(const SharedPtr& other) : data_(other.data_), counter_(other.counter_) {
+    if (counter_ != nullptr) {
+      ++*counter_;
+    }
+  }
+
+  SharedPtr(SharedPtr&& other) : data_(other.data_), counter_(other.counter_) {
+    other.data_ = nullptr;
+    other.counter_ = nullptr;
+  }
+
+  SharedPtr& operator=(const SharedPtr& other) {
+    Destroy();
+
+    data_ = other.data_;
+    counter_ = other.counter_;
+    if (counter_ != nullptr) {
+      ++*counter_;
+    }
+
+    return *this;
+  }
+
+  SharedPtr& operator=(SharedPtr&& other) {
+    Destroy();
+
+    data_ = other.data_;
+    counter_ = other.counter_;
+
+    return *this;
+  }
+
+  ~SharedPtr() {
+    Destroy();
+  }
+
+  [[nodiscard]] std::size_t UseCount() const noexcept {
+    return counter_ == nullptr ? 0u : *counter_;
+  }
+
+  [[nodiscard]] T* GetData() const noexcept {
+    return data_;
+  }
+};
+}  // namespace algo::pointers
---
+++ cpp/tests/shared_ptr_test.cpp
@@ -0,0 +1,73 @@
+#include <catch2/catch.hpp>
+
+#include "shared_ptr.hpp"
+#include <functional>
+
+TEST_CASE("Создание умного указателя SharedPtr", "[shared_ptr_build]") {
+  using namespace algo::pointers;
+  SECTION("Проверка корректности для конструкторов и операторов пустого SharedPtr") {
+    SharedPtr<int> shared;
+    REQUIRE(shared.UseCount() == 0);
+
+    SharedPtr<int> shared_copy_constructor = shared;
+    REQUIRE(shared.UseCount() == 0);
+    REQUIRE(shared_copy_constructor.UseCount() == 0);
+
+    SharedPtr<int> shared_move_constructor = std::move(shared);
+    REQUIRE(shared.UseCount() == 0);
+    REQUIRE(shared_move_constructor.UseCount() == 0);
+
+    SharedPtr<int> shared_copy_operator;
+    shared_copy_operator = shared;
+    REQUIRE(shared.UseCount() == 0);
+    REQUIRE(shared_copy_operator.UseCount() == 0);
+
+    SharedPtr<int> shared_move_operator;
+    shared_move_operator = std::move(shared);
+    REQUIRE(shared.UseCount() == 0);
+    REQUIRE(shared_move_operator.UseCount() == 0);
+  }
+
+  SECTION("Конструктор копирования SharedPtr") {
+    SharedPtr<int> shared{new int{1}};
+    SharedPtr<int> copy_shared = shared;
+    REQUIRE(shared.UseCount() == 2);
+    REQUIRE(copy_shared.UseCount() == 2);
+    REQUIRE(copy_shared.GetData() == shared.GetData());
+  }
+
+  SECTION("Оператор копирования SharedPtr") {
+    SharedPtr<int> shared{new int{1}};
+    SharedPtr<int> copy_shared;
+    copy_shared = shared;
+
+    REQUIRE(shared.UseCount() == 2);
+    REQUIRE(copy_shared.UseCount() == 2);
+    REQUIRE(copy_shared.GetData() == shared.GetData());
+
+    class Local final {
+      bool* destroyed_{nullptr};
+
+     public:
+      Local() noexcept = default;
+      explicit Local(bool* destroyed) noexcept : destroyed_(destroyed) {
+      }
+      ~Local() noexcept {
+        if (destroyed_ != nullptr) {
+          *destroyed_ = true;
+        }
+      }
+    };
+
+    bool is_destroyed = false;
+    SharedPtr<Local> shared_local_1{new Local{&is_destroyed}};
+    SharedPtr<Local> shared_local_2{new Local{}};
+    REQUIRE(shared_local_1.UseCount() == 1);
+    REQUIRE(shared_local_2.UseCount() == 1);
+    shared_local_1 = shared_local_2;
+    REQUIRE(is_destroyed);
+    REQUIRE(shared_local_1.UseCount() == 2);
+    REQUIRE(shared_local_2.UseCount() == 2);
+    REQUIRE(shared_local_1.GetData() == shared_local_2.GetData());
+  }
+}
