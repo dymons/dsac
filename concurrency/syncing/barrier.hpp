8eab585acaf2ac4cd8d2f9fcc5d23cf918d64cd6
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2021-11-13 17:45:38 +0300

    Реализовал идею Pimpl для Barrier

--- concurrency/syncing/barrier.hpp
+++ concurrency/syncing/barrier.hpp
@@ -1,19 +1,9 @@
 #pragma once
 
-#include <mutex>
-#include <condition_variable>
+#include <memory>
 
 namespace algo::syncing {
 class Barrier final {
-  std::mutex mutex_;
-  std::condition_variable cv_;
-
-  const std::size_t size_awaited_threads_;
-  std::size_t current_awaited_threads_;
-
-  enum class State : unsigned char { Blocking, Unblocking };
-  State state_;
-
  public:
   explicit Barrier(std::size_t size_awaited_threads);
   Barrier(const Barrier&) = delete;
@@ -22,5 +12,9 @@ class Barrier final {
   Barrier& operator=(Barrier&&) = delete;
 
   void ArriveAndWait();
+
+ private:
+  class Pimpl;
+  std::shared_ptr<Pimpl> pimpl_;
 };
 }  // namespace algo::syncing
