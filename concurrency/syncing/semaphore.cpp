986e82be867abee4a55ad65643949183bafd6a89
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2021-12-29 12:17:48 +0300

    Добавил простую реализацию Semaphore

--- concurrency/syncing/semaphore.cpp
+++ concurrency/syncing/semaphore.cpp
@@ -1,1 +1,19 @@
 #include <concurrency/syncing/semaphore.hpp>
+
+namespace algo::syncing {
+Semaphore::Semaphore(std::size_t consumers) : tokens_(consumers) {
+}
+
+void Semaphore::Acquire() {
+  std::unique_lock guard(mutex_);
+  not_zero_.wait(guard, [this]() {
+    return tokens_.load() != 0U;
+  });
+  --tokens_;
+}
+
+void Semaphore::Release() {
+  ++tokens_;
+  not_zero_.notify_one();
+}
+}  // namespace algo::syncing
--- concurrency/syncing/semaphore.hpp
+++ concurrency/syncing/semaphore.hpp
@@ -1,3 +1,20 @@
 #pragma once
 
-namespace algo::syncing {}  // namespace algo::syncing
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+namespace algo::syncing {
+class Semaphore final {
+ public:
+  explicit Semaphore(std::size_t);
+
+  void Acquire();
+  void Release();
+
+ private:
+  std::atomic<std::size_t> tokens_;
+  std::mutex mutex_;
+  std::condition_variable not_zero_;
+};
+}  // namespace algo::syncing
