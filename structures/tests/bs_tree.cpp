475de57b587a3e39fda639a4d4d09d67762fceb8
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-03-07 01:02:21 +0300

    1. Реализовал итератор для бинарного дерева поиска 2. Добавил тесты на проверку создания дерева и итераторов

---
+++ structures/tests/bs_tree.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch.hpp>
+
+#include <structures/tree/bs_tree.hpp>
+
+TEST_CASE("Создание бинарного дерева поиска", "[binary_search_tree][construct]") {
+  using namespace algo::tree;
+
+  SECTION("Проверка метода size для пустого дерева") {
+    BinarySearchTree<int> tree;
+    REQUIRE(tree.Size() == 0);
+  }
+  SECTION("Проверка метода empty для пустого дерева") {
+    BinarySearchTree<int> tree;
+    REQUIRE(tree.IsEmpty());
+  }
+}
+
+TEST_CASE("Создание итератора для бинарного дерева поиска",
+          "[binary_search_tree][iterator]") {
+  using namespace algo::tree;
+
+  SECTION("Проверка итераторов пустого дерева на равенство") {
+    BinarySearchTree<int> tree;
+    auto begin(tree.begin());
+    auto end(tree.end());
+    REQUIRE(begin == end);
+  }
+  SECTION("Проверка константных итераторов пустого дерева на равенство") {
+    BinarySearchTree<int> tree;
+    auto begin(tree.cbegin());
+    auto end(tree.cend());
+    REQUIRE(begin == end);
+  }
+}
--- structures/tree/bs_tree.hpp
+++ structures/tree/bs_tree.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <functional>
 #include <memory>
+#include <iterator>
 
 namespace algo::tree {
-
-template <typename Key, typename Compare, typename Allocator = std::allocator<Key>>
+template <typename Key, typename Compare = std::less<Key>,
+          typename Allocator = std::allocator<Key>>
 class BinarySearchTree final {
   using AllocatorTraits = std::allocator_traits<Allocator>;
 
@@ -33,12 +35,29 @@ class BinarySearchTree final {
 
   template <typename NodeAllocator>
   class BinarySearchTreeNodeDestructor final {
+   public:
     using allocator_type = NodeAllocator;
     using allocator_traits = std::allocator_traits<allocator_type>;
     using value_type = typename allocator_traits::value_type;
+    using pointer = typename std::allocator_traits<allocator_type>::pointer;
+
+   private:
+    allocator_type& node_allocator_;
 
    public:
-    using pointer = typename std::allocator_traits<allocator_type>::pointer;
+    bool value_constructed_;
+    explicit BinarySearchTreeNodeDestructor(allocator_type& node_allocator) noexcept
+        : node_allocator_(node_allocator), value_constructed_(false) {
+    }
+
+    void operator()(pointer p) noexcept {
+      if (value_constructed_) {
+        allocator_traits::destroy(node_allocator_, std::addressof(p->key_));
+      }
+      if (p) {
+        allocator_traits::deallocate(node_allocator_, p, 1);
+      }
+    }
   };
 
  public:
@@ -63,5 +82,164 @@ class BinarySearchTree final {
   using const_node_pointer = typename node_traits::const_pointer;
   using node_destructor = BinarySearchTreeNodeDestructor<node_allocator>;
   using node_holder = std::unique_ptr<node, node_destructor>;
+
+ public:
+  template <typename BinarySearchTreeNode>
+  class BinarySearchTreeIterator final
+      : std::iterator<std::bidirectional_iterator_tag,
+                      typename BinarySearchTreeNode::pointer> {
+    using key_type = typename BinarySearchTreeNode::key_type;
+    using node_pointer = typename BinarySearchTreeNode::pointer;
+
+    template <typename, typename, typename>
+    friend class BinarySearchTree;
+
+   public:
+    using iterator_category = const std::bidirectional_iterator_tag;
+    using value_type = typename BinarySearchTreeNode::value_type;
+    using const_reference = typename BinarySearchTreeNode::const_reference;
+
+   private:
+    bool IsLeftChild(node_pointer node) const noexcept {
+      return (node == static_cast<node_pointer>(node->parent_->left_));
+    }
+
+    node_pointer FindMinNode(node_pointer node) noexcept {
+      while (node != nullptr && node->left_ != nullptr) {
+        node = node->left_;
+      }
+      return node;
+    }
+
+    node_pointer FindMaxNode(node_pointer node) noexcept {
+      while (node != nullptr && node->right_ != nullptr) {
+        node = node->right_;
+      }
+      return node;
+    }
+
+    node_pointer FindNextNode(node_pointer node) {
+      if (node != nullptr && node->right_ != nullptr) {
+        return FindMinNode(node->right_);
+      }
+
+      while (!IsLeftChild(node)) {
+        node = node->parent_;
+      }
+
+      return node->parent_;
+    }
+
+    node_pointer current_node_;
+
+   public:
+    const_reference operator*() const {
+      return current_node_->key_;
+    }
+
+    BinarySearchTreeIterator& operator++() {
+      current_node_ = FindNextNode(current_node_);
+      return *this;
+    }
+
+    BinarySearchTreeIterator operator++(int) {
+      auto latest_iterator = *this;
+      ++*this;
+      return latest_iterator;
+    }
+
+    BinarySearchTreeIterator operator--() = delete;
+    BinarySearchTreeIterator operator--(int) = delete;
+
+    friend bool operator==(const BinarySearchTreeIterator& lhs,
+                           const BinarySearchTreeIterator& rhs) {
+      return lhs.current_node_ == rhs.current_node_;
+    }
+
+    friend bool operator!=(const BinarySearchTreeIterator& lhs,
+                           const BinarySearchTreeIterator& rhs) {
+      return !(lhs == rhs);
+    }
+
+    BinarySearchTreeIterator() = delete;
+    BinarySearchTreeIterator(const BinarySearchTreeIterator&) = default;
+    BinarySearchTreeIterator(BinarySearchTreeIterator&&) = default;
+    BinarySearchTreeIterator& operator=(const BinarySearchTreeIterator&) = default;
+    BinarySearchTreeIterator& operator=(BinarySearchTreeIterator&&) = default;
+    ~BinarySearchTreeIterator() = default;
+
+   private:
+    explicit BinarySearchTreeIterator(const node_pointer& node) : current_node_(node) {
+    }
+  };
+
+ public:
+  using iterator = BinarySearchTreeIterator<node>;
+  using const_iterator = BinarySearchTreeIterator<const_node>;
+
+  explicit BinarySearchTree(const Compare& comp = Compare(),
+                            const Allocator& alloc = Allocator())
+      : compare_(comp),
+        allocator_(node_allocator(alloc)),
+        size_(0),
+        header_(MakeNodeHolder(value_type{}).release()) {
+    this->header_->left_ = this->header_;
+    this->header_->right_ = this->header_;
+  };
+
+  ~BinarySearchTree() = default;
+
+  iterator begin() noexcept {  // NOLINT(readability-identifier-naming)
+    return MakeIteratorBy(header_->left_);
+  }
+
+  const_iterator begin() const noexcept {  // NOLINT(readability-identifier-naming)
+    return MakeIteratorBy(header_->left_);
+  }
+
+  const_iterator cbegin() const noexcept {  // NOLINT(readability-identifier-naming)
+    return MakeIteratorBy(header_->left_);
+  }
+
+  iterator end() noexcept {  // NOLINT(readability-identifier-naming)
+    return MakeIteratorBy(header_);
+  }
+
+  const_iterator end() const noexcept {  // NOLINT(readability-identifier-naming)
+    return MakeIteratorBy(header_);
+  }
+
+  const_iterator cend() const noexcept {  // NOLINT(readability-identifier-naming)
+    return MakeIteratorBy(header_);
+  }
+
+  inline bool IsEmpty() const noexcept {  // NOLINT(readability-identifier-naming)
+    return Size() == 0;
+  }
+
+  inline size_t Size() const noexcept {  // NOLINT(readability-identifier-naming)
+    return size_;
+  }
+
+ private:
+  const key_compare compare_;
+  node_allocator allocator_;
+  size_t size_;
+  node_pointer header_;
+
+  iterator MakeIteratorBy(node_pointer& node) noexcept {
+    return iterator(node);
+  }
+
+  const_iterator MakeIteratorBy(const node_pointer& node) const noexcept {
+    return const_iterator(node);
+  }
+
+  node_holder MakeNodeHolder(value_type&& value) {
+    node_holder nh(allocator_.allocate(1), node_destructor(allocator_));
+    node_traits::construct(allocator_, nh.get(), std::forward<value_type>(value));
+    nh.get_deleter().value_constructed_ = true;
+    return nh;
+  }
 };
 }  // namespace algo::tree
