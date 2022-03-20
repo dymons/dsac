d2a2d34622f3b4267e823266580afba4978ea6d7
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-03-20 19:13:15 +0300

    Update CMakeLists.txt

--- docs/conf.py
+++ docs/conf.py
@@ -1,61 +1,19 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-import subprocess, os
-
-def configureDoxyfile(input_dir, output_dir):
-
-    with open('Doxyfile.in', 'r') as file :
-        filedata = file.read()
-
-    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
-    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
-
-    with open('Doxyfile', 'w') as file:
-        file.write(filedata)
-
-# Check if we're running on Read the Docs' servers
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-breathe_projects = {}
-if read_the_docs_build:
-    input_dir = '../dsac'
-    output_dir = 'build'
-    configureDoxyfile(input_dir, output_dir)
-    subprocess.call('doxygen', shell=True)
-    breathe_projects['dsac'] = output_dir + '/xml'
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'dsac'
-copyright = '2022, Dmitry Emelyanov'
+copyright = 'Copyright (c) 2022, The Dsac Core developers'
 author = 'Dmitry Emelyanov'
-
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#...
+# ...
 
-extensions = [ "breathe" ]
+extensions = ["breathe"]
 
-#...
+# ...
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -64,7 +22,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
