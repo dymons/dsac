73cc35db3c6955a0c42fd7be4bc8caef0ad0f046
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-03-26 21:34:22 +0300

    Change politic to policy

--- dsac/include/dsac/algorithm/binary_tree_sort/binary_tree_sort_seq.hpp
+++ dsac/include/dsac/algorithm/binary_tree_sort/binary_tree_sort_seq.hpp
@@ -8,9 +8,9 @@ namespace dsac {
 template <typename ForwardIterator>
 void binary_tree_sort(ForwardIterator first, ForwardIterator last)
 {
-  using value_type          = typename dsac::iterator_traits<ForwardIterator>::value_type;
-  using binary_tree_politic = binary_search_tree<value_type>;
-  detail::binary_tree_sort<binary_tree_politic>(first, last);
+  using value_type         = typename dsac::iterator_traits<ForwardIterator>::value_type;
+  using binary_tree_policy = binary_search_tree<value_type>;
+  detail::binary_tree_sort<binary_tree_policy>(first, last);
 }
 }  // namespace dsac
 
--- dsac/include/dsac/algorithm/datail/binary_tree_sort.hpp
+++ dsac/include/dsac/algorithm/datail/binary_tree_sort.hpp
@@ -3,13 +3,28 @@
 #include <algorithm>
 
 namespace dsac::detail {
-template <class BinaryTreePolitic, typename ForwardIterator>
+
+/*!
+    \brief
+        Tree sort is a sorting algorithm that is based on Binary Search Tree data structure.
+
+    \param first
+        Iterator to the initial position in the sequence
+    \param last
+        Iterator to the final position in the sequence
+
+    \par Worst Case Complexity:
+        Time Complexity \p O(N) and Space Complexity \p O(N)
+
+    \ingroup DsacAlgorithms
+*/
+template <class BinaryTreePolicy, typename ForwardIterator>
 void binary_tree_sort(ForwardIterator first, ForwardIterator last)
 {
   using std::begin;
   using std::end;
 
-  BinaryTreePolitic tree(first, last);
+  BinaryTreePolicy tree(first, last);
   std::copy(begin(tree), end(tree), first);
 }
 }  // namespace dsac::detail
