925592c1e5d9eb12f84dfd5f5e1b3f06fd0acf7b
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-03-27 15:37:08 +0300

    Add testcase for binary_tree_sort with using binary_tree_policy

--- dsac/test/algorithm/binary_tree_sort_test.cpp
+++ dsac/test/algorithm/binary_tree_sort_test.cpp
@@ -28,9 +28,18 @@ TEST_CASE("Testcases are sorted using binary tree sort (D)", "[binary_tree_sort]
 
     SECTION("Using custom predicate")
     {
-      dsac::binary_tree_sort(sorted_testcase, std::greater<int>{});
-      REQUIRE(dsac::is_sorted(sorted_testcase, std::greater<int>{}));
+      dsac::binary_tree_sort(sorted_testcase, std::greater<>{});
+      REQUIRE(dsac::is_sorted(sorted_testcase, std::greater<>{}));
     }
+  }
+
+  SECTION("Sorting a testcase with custom policy")
+  {
+    auto some_testcase = testcase{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    using binary_tree_policy = dsac::binary_search_tree<int, std::greater<>>;
+    dsac::binary_tree_sort(begin(some_testcase), end(some_testcase), binary_tree_policy{});
+    REQUIRE(dsac::is_sorted(some_testcase, std::greater<>{}));
   }
 }
 
