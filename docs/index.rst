95114df299e96c7a189b8e779a1a958e9888eb9b
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-03-27 10:22:17 +0300

    Add documentation for binary_tree_sort

--- docs/index.rst
+++ docs/index.rst
@@ -3,7 +3,9 @@ Welcome to dsac's documentation!
 
 .. toctree::
 
-.. doxygenfunction:: dsac::detail::is_sorted(ForwardIterator begin, ForwardIterator end, BinaryPredicate predicate)
+.. doxygenfunction:: dsac::detail::is_sorted(ForwardIterator begin, ForwardIterator end, Compare comp)
+.. doxygenfunction:: dsac::detail::binary_tree_sort(ForwardIterator begin, ForwardIterator end)
+
 
 Indices and tables
 ==================
--- dsac/include/dsac/algorithm/datail/binary_tree_sort.hpp
+++ dsac/include/dsac/algorithm/datail/binary_tree_sort.hpp
@@ -27,15 +27,26 @@ namespace dsac::detail {
     \brief
         Tree sort is a sorting algorithm that is based on Binary Search Tree data structure.
 
-    \param first
-        Iterator to the initial position in the sequence
-    \param last
-        Iterator to the final position in the sequence
+    \tparam BinaryTreePolicy
+        A policy class that allows you to combine different behaviors to create a rich variety of
+   binary sorting algorithm combinations. \param first Iterator to the initial position in the
+   sequence \param last Iterator to the final position in the sequence
+
+    \throw DublicatesNotSupported
+        The algorithm does not support working with duplicate values at the moment
 
     \par Worst Case Complexity:
         Time Complexity \p O(N) and Space Complexity \p O(N)
 
     \ingroup DsacAlgorithms
+
+    \code
+        using binary_tree_policy = dsac::binary_search_tree<int>;
+        dsac::binary_tree_sort<binary_tree_policy>(begin(testcase), end(testcase));
+
+        using binary_tree_policy = dsac::red_black_tree<int, std::greater<int>>;
+        dsac::binary_tree_sort<binary_tree_policy>(begin(testcase), end(testcase));
+    \endcode
 */
 template <class BinaryTreePolicy, typename ForwardIterator>
 void binary_tree_sort(ForwardIterator first, ForwardIterator last)
