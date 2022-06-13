eeef508742b6a1d9785421ca5ed2f0fcb45e6d11
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-06-13 15:25:11 +0300

    Add more tests for lexical analyzer

--- dsac/test/container/test_graphviz_lexical_analyzer.cpp
+++ dsac/test/container/test_graphviz_lexical_analyzer.cpp
@@ -5,22 +5,68 @@
 using namespace std::literals;
 
 TEST_CASE("Checking the analytical analysis of the graphviz for recognition of tokens", "[graphviz][lexical]") {
-  constexpr char const* kDigraph = R"graph(
-    digraph abc {
-      a -> b -> c;
-    }
-  )graph";
+  SECTION("Check empty digraph") {
+    constexpr char const* kEmptyDigraph = R"graph(
+      digraph abc {}
+    )graph";
 
-  dsac::graphviz_lexical_analyzer tokenizer(kDigraph);
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::keyword, "digraph"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "abc"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "{"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "a"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::operator_, "->"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "b"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::operator_, "->"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "c"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, ";"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "}"sv));
-  REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::eof, ""sv));
+    dsac::graphviz_lexical_analyzer tokenizer(kEmptyDigraph);
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::keyword, "digraph"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "abc"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "{"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "}"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::eof, ""sv));
+  }
+  SECTION("Check empty graph") {
+    constexpr char const* kEmptyGraph = R"graph(
+      graph abc {}
+    )graph";
+
+    dsac::graphviz_lexical_analyzer tokenizer(kEmptyGraph);
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::keyword, "graph"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "abc"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "{"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "}"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::eof, ""sv));
+  }
+  SECTION("Check digraph with one statement") {
+    constexpr char const* kDigraph = R"graph(
+      digraph abc {
+        a -> b -> c;
+      }
+    )graph";
+
+    dsac::graphviz_lexical_analyzer tokenizer(kDigraph);
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::keyword, "digraph"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "abc"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "{"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "a"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::operator_, "->"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "b"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::operator_, "->"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "c"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, ";"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "}"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::eof, ""sv));
+  }
+  SECTION("Check graph with one statement") {
+    constexpr char const* kGraph = R"graph(
+      graph abc {
+        a -- b -- c;
+      }
+    )graph";
+
+    dsac::graphviz_lexical_analyzer tokenizer(kGraph);
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::keyword, "graph"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "abc"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "{"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "a"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::operator_, "--"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "b"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::operator_, "--"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::identifier, "c"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, ";"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::punctuator, "}"sv));
+    REQUIRE(tokenizer.get_next_token() == std::make_pair(dsac::token::eof, ""sv));
+  }
 }
