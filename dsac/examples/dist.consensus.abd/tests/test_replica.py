9bf6ba37eae29882c434a32a2a7c8049fb7bd510
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-02 22:41:18 +0300

    Refactoring code

--- dsac/examples/dist.consensus.abd/tests/test_replica.py
+++ dsac/examples/dist.consensus.abd/tests/test_replica.py
@@ -3,4 +3,6 @@ async def test_replica_get(consensus):
         'key': 'entity_0',
     })
     assert response.status == 200
-    assert response.json() == {'value': '__unspecified_value'}
+    assert response.json() == {
+        'value': '__unspecified_value'
+    }
