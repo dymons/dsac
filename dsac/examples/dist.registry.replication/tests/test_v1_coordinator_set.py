774220c79b1af35b481c0d1b8b9e9a6147e1a6e5
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-04 21:48:42 +0300

    Add documentation

--- dsac/examples/dist.registry.replication/tests/test_v1_coordinator_set.py
+++ dsac/examples/dist.registry.replication/tests/test_v1_coordinator_set.py
@@ -9,7 +9,7 @@ async def test_happy_path(registry, snapshot):
     ]
 
 
-async def test_ignoring_value_with_lowest_timestamp(registry, snapshot):
+async def test_reject_record_with_olden_timestamp(registry, snapshot):
     """
                           snapshot            snapshot
                               |                  |
@@ -24,8 +24,10 @@ async def test_ignoring_value_with_lowest_timestamp(registry, snapshot):
         -----------\|---------|------------------|------
                               |                  |
                               ^                  ^
-          Replicas in the cluster contain        Record has the lowest timestamp value,
-          a value "12" with a timestamp "2"      so the record will not be executed
+          Replicas in the cluster contain        Record has olden timestamp value,
+          a value "12" with a timestamp "2"      so the record will not be executed.
+                                                 Replicas in the cluster contain
+                                                 a value "12" with a timestamp "2"
     """
     assert (await registry[8080].post('v1/coordinator/set', json={'value': 10, 'timestamp': 0})).status == 200
     assert (await registry[8080].post('v1/coordinator/set', json={'value': 11, 'timestamp': 1})).status == 200
