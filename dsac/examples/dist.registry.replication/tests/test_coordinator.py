31285f47bcfa567e5cc09cede7b0f492b46decfa
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-04 20:58:30 +0300

    Refactoring tests

--- dsac/examples/dist.registry.replication/tests/test_coordinator.py
+++ dsac/examples/dist.registry.replication/tests/test_coordinator.py
@@ -5,9 +5,10 @@ async def test_coordinator_set_happy_path(registry):
     })
     assert response.status == 200
 
-    response = await registry[8080].post('v1/coordinator/get', json={})
-    assert response.status == 200
-    assert response.json() == {
-        'value': 10,
-        'timestamp': 0,
-    }
+    for port in range(8080, 8082):
+        response = await registry[port].post('v1/coordinator/get', json={})
+        assert response.status == 200
+        assert response.json() == {
+            'value': 10,
+            'timestamp': 0,
+        }
