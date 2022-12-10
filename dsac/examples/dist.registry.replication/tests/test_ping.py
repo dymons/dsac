423fd2b073fb94862bbb4c88143422fb5ca4f9e2
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-10 15:10:20 +0300

    Update test_ping.py

--- dsac/examples/dist.registry.replication/tests/test_ping.py
+++ dsac/examples/dist.registry.replication/tests/test_ping.py
@@ -1,9 +1,4 @@
 async def test_ping(registry):
-    response = await registry[8080].get('ping')
-    assert response.status == 200
-
-    response = await registry[8081].get('ping')
-    assert response.status == 200
-
-    response = await registry[8082].get('ping')
-    assert response.status == 200
+    assert (await registry[8080].get('ping')).status == 200
+    assert (await registry[8081].get('ping')).status == 200
+    assert (await registry[8082].get('ping')).status == 200
