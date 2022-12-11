636736b1674a53c3ce11ab3c3682aed72aac9e48
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-11 12:28:26 +0300

    Writing tests on testsuite

--- dsac/examples/dist.registry.replication/tests/test_v1_replica_read.py
+++ dsac/examples/dist.registry.replication/tests/test_v1_replica_read.py
@@ -19,3 +19,65 @@ async def test_read_from_register_replica_with_unspecified_state(
     response = await registry[8082].post('v1/replica/read')
     assert response.status_code == 404
     assert response.json() == {'error_message': 'The register is not initialized'}
+
+
+async def test_read_from_register_replica_with_specified_state(
+        registry,
+        snapshot,
+):
+    response = await registry[8080].post('v1/replica/write', json={
+        'value': 10,
+        'timestamp': 0,
+    })
+    assert response.status_code == 200
+
+    response = await registry[8080].post('v1/replica/read')
+    assert response.status_code == 200
+    assert response.json() == {
+        'value': 10,
+        'timestamp': 0,
+    }
+
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': {'timestamp': 0, 'value': 10}},
+        {'port': 8081, 'snapshot': None},
+        {'port': 8082, 'snapshot': None},
+    ]
+
+    response = await registry[8081].post('v1/replica/write', json={
+        'value': 20,
+        'timestamp': 1,
+    })
+    assert response.status_code == 200
+
+    response = await registry[8081].post('v1/replica/read')
+    assert response.status_code == 200
+    assert response.json() == {
+        'value': 20,
+        'timestamp': 1,
+    }
+
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': {'timestamp': 0, 'value': 10}},
+        {'port': 8081, 'snapshot': {'timestamp': 1, 'value': 20}},
+        {'port': 8082, 'snapshot': None},
+    ]
+
+    response = await registry[8082].post('v1/replica/write', json={
+        'value': 30,
+        'timestamp': 2,
+    })
+    assert response.status_code == 200
+
+    response = await registry[8082].post('v1/replica/read')
+    assert response.status_code == 200
+    assert response.json() == {
+        'value': 30,
+        'timestamp': 2,
+    }
+    
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': {'timestamp': 0, 'value': 10}},
+        {'port': 8081, 'snapshot': {'timestamp': 1, 'value': 20}},
+        {'port': 8082, 'snapshot': {'timestamp': 2, 'value': 30}},
+    ]
