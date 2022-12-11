6f63ac019b178bc8b4d47766764e339a725f675a
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-11 12:11:37 +0300

    Writing tests on testsuite

--- dsac/examples/dist.registry.replication/tests/test_v1_replica_reset.py
+++ dsac/examples/dist.registry.replication/tests/test_v1_replica_reset.py
@@ -1,4 +1,21 @@
-async def test_reset_register_replica(registry, snapshot):
+async def test_reset_register_replica_with_unspecified_state(registry, snapshot):
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': None},
+        {'port': 8081, 'snapshot': None},
+        {'port': 8082, 'snapshot': None},
+    ]
+
+    assert (await registry[8080].post('v1/replica/reset')).status == 200
+    assert (await registry[8081].post('v1/replica/reset')).status == 200
+    assert (await registry[8082].post('v1/replica/reset')).status == 200
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': None},
+        {'port': 8081, 'snapshot': None},
+        {'port': 8082, 'snapshot': None},
+    ]
+
+
+async def test_reset_register_replica_with_specified_state(registry, snapshot):
     assert (await registry[8080].post('v1/replica/write', json={'value': 10, 'timestamp': 10})).status == 200
     assert (await registry[8081].post('v1/replica/write', json={'value': 10, 'timestamp': 10})).status == 200
     assert (await registry[8082].post('v1/replica/write', json={'value': 10, 'timestamp': 10})).status == 200
