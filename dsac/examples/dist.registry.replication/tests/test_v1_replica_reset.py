e28f59ef54f21a25396fc22a04200a316facb889
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-11 12:09:43 +0300

    Writing tests on testsuite

---
+++ dsac/examples/dist.registry.replication/tests/test_v1_replica_reset.py
@@ -0,0 +1,18 @@
+async def test_reset_register_replica(registry, snapshot):
+    assert (await registry[8080].post('v1/replica/write', json={'value': 10, 'timestamp': 10})).status == 200
+    assert (await registry[8081].post('v1/replica/write', json={'value': 10, 'timestamp': 10})).status == 200
+    assert (await registry[8082].post('v1/replica/write', json={'value': 10, 'timestamp': 10})).status == 200
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': {'timestamp': 10, 'value': 10}},
+        {'port': 8081, 'snapshot': {'timestamp': 10, 'value': 10}},
+        {'port': 8082, 'snapshot': {'timestamp': 10, 'value': 10}},
+    ]
+
+    assert (await registry[8080].post('v1/replica/reset')).status == 200
+    assert (await registry[8081].post('v1/replica/reset')).status == 200
+    assert (await registry[8082].post('v1/replica/reset')).status == 200
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': None},
+        {'port': 8081, 'snapshot': None},
+        {'port': 8082, 'snapshot': None},
+    ]
