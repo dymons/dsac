cdb3bdc5d4ed31c23eb534fcd2e37dfd6a035468
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-11 18:50:19 +0300

    Update test

--- dsac/examples/dist.registry.replication/tests/test_spec_read_to_write_phase.py
+++ dsac/examples/dist.registry.replication/tests/test_spec_read_to_write_phase.py
@@ -24,6 +24,12 @@ async def test_spec_read_to_write_phase(registry, snapshot):
 
     assert (await registry[8081].post('v1/replica/write', json={'value': 10, 'timestamp': 1})).status_code == 200
 
+    assert await snapshot() == [
+        {'port': 8080, 'snapshot': None},
+        {'port': 8081, 'snapshot': {'timestamp': 1, 'value': 10}},
+        {'port': 8082, 'snapshot': None},
+    ]
+
     response = await registry[8080].post('v1/coordinator/read')
     assert response.status_code == 200
     assert response.json() == {
