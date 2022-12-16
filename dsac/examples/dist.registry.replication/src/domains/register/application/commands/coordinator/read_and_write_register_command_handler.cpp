2d8de4c9289fff867792282ce67d50f9114a67f6
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-16 22:33:58 +0300

    Refactoring code

--- dsac/examples/dist.registry.replication/src/domains/register/application/commands/coordinator/read_and_write_register_command_handler.cpp
+++ dsac/examples/dist.registry.replication/src/domains/register/application/commands/coordinator/read_and_write_register_command_handler.cpp
@@ -14,51 +14,97 @@ using presentation::web::register_replica_client;
 
 namespace {
 
-auto create_cluster_snapshot(executor_base_ref const& executor, domain::policy::quorum_policy_ref const& quorum_policy)
-    -> dynamic_array<result<register_dto>> {
+class cluster_exception : public std::runtime_error {
+public:
+  using std::runtime_error::runtime_error;
+};
+
+class not_found_latest_value final : public cluster_exception {
+public:
+  not_found_latest_value()
+    : cluster_exception("Couldn't extract the current register value") {
+  }
+};
+
+class not_found_latest_timestamp final : public cluster_exception {
+public:
+  not_found_latest_timestamp()
+    : cluster_exception("Couldn't extract the current register timestamp") {
+  }
+};
+
+class cluster_container final {
+  dynamic_array<result<register_dto>> snapshots_;
+  std::optional<register_dto>         latest_snapshot_;
+
+  [[nodiscard]] auto choose_latest_snapshot() const -> std::optional<register_dto> {
+    if (snapshots_.empty()) {
+      return std::nullopt;
+    }
+
+    auto latest_snapshot = snapshots_.begin();
+    for (auto begin = std::begin(snapshots_), end = std::end(snapshots_); begin != end; ++begin) {
+      if (*begin > *latest_snapshot || !latest_snapshot->has_value()) {
+        latest_snapshot = begin;
+      }
+    }
+
+    if (!latest_snapshot->has_value()) {
+      return std::nullopt;
+    }
+
+    return latest_snapshot->value_or_throw();
+  }
+
+public:
+  /*implicit*/ cluster_container(dynamic_array<result<register_dto>> snapshots)  // NOLINT(google-explicit-constructor)
+    : snapshots_(std::move(snapshots))
+    , latest_snapshot_(choose_latest_snapshot()) {
+  }
+
+  [[nodiscard]] auto has_latest_snapshot() const -> bool try { return latest_snapshot_.has_value(); } catch (...) {
+    return false;
+  }
+
+  [[nodiscard]] auto is_consistent() const -> bool {
+    return std::ranges::all_of(snapshots_, [this](auto&& snapshot) {
+      return snapshot.has_value() && (snapshot.value_or_throw() == latest_snapshot_);
+    });
+  }
+
+  [[nodiscard]] auto get_latest_value() const -> std::int32_t {
+    if (not latest_snapshot_.has_value()) [[unlikely]] {
+      throw not_found_latest_value{};
+    }
+    return latest_snapshot_->get_value();
+  }
+
+  [[nodiscard]] auto get_latest_timestamp() const -> std::size_t {
+    if (not latest_snapshot_.has_value()) [[unlikely]] {
+      throw not_found_latest_timestamp{};
+    }
+    return latest_snapshot_->get_timestamp();
+  }
+};
+
+auto create_cluster_snapshot(auto&& executor, auto&& quorum_policy) -> cluster_container {
   dynamic_array<future<register_dto>> responses;
   for (std::string const& key : register_replica_client::factory::get_registered_keys()) {
     std::unique_ptr<register_replica_client> replica = register_replica_client::factory::construct(key, executor);
     responses.push_back(replica->read());
   }
 
-  std::size_t const                           quorum        = quorum_policy->quorum(responses.size());
-  future<dynamic_array<result<register_dto>>> quorum_future = first_n(std::move(responses), quorum);
+  auto const quorum        = quorum_policy->quorum(responses.size());
+  auto       quorum_future = first_n(std::move(responses), quorum);
 
   return std::move(quorum_future).get().value_or_throw();
 }
 
-auto choose_latest_snapshot(dynamic_array<result<register_dto>> const& snapshots) -> std::optional<register_dto> {
-  if (snapshots.empty()) {
-    return std::nullopt;
-  }
-
-  auto latest_snapshot = snapshots.begin();
-  for (auto begin = std::begin(snapshots), end = std::end(snapshots); begin != end; ++begin) {
-    if (*begin > *latest_snapshot || !latest_snapshot->has_value()) {
-      latest_snapshot = begin;
-    }
-  }
-
-  if (!latest_snapshot->has_value()) {
-    return std::nullopt;
-  }
-
-  return latest_snapshot->value_or_throw();
-}
-
-auto make_write_phase_if_need(
-    dynamic_array<result<register_dto>> const& snapshots,
-    register_dto const&                        latest_snapshot,
-    executor_base_ref const&                   executor,
-    domain::policy::quorum_policy_ref const&   quorum_policy) -> void {
-  auto const is_cluster_consistent = std::ranges::all_of(snapshots, [&latest_snapshot](auto&& snapshot) {
-    return snapshot.has_value() && (snapshot.value_or_throw() == latest_snapshot);
-  });
-  if (not is_cluster_consistent) {
+auto make_write_phase_if_need(auto&& cluster_snapshot, auto&& executor, auto&& quorum_policy) -> void {
+  if (not cluster_snapshot.is_consistent()) {
     write_register_command write_register_command{};
-    write_register_command.value     = latest_snapshot.get_value();
-    write_register_command.timestamp = latest_snapshot.get_timestamp();
+    write_register_command.value     = cluster_snapshot.get_latest_value();
+    write_register_command.timestamp = cluster_snapshot.get_latest_timestamp();
 
     write_register_command_handler write_register_command_handler{executor, quorum_policy};
     write_register_command_handler.handle(write_register_command);
@@ -67,14 +113,17 @@ auto make_write_phase_if_need(
 
 }  // namespace
 
-auto read_and_write_register_command_handler::handle() const -> std::optional<register_dto> {
-  auto const snapshots       = create_cluster_snapshot(executor_, quorum_policy_);
-  auto const latest_snapshot = choose_latest_snapshot(snapshots);
-  if (latest_snapshot.has_value()) {
-    make_write_phase_if_need(snapshots, latest_snapshot.value(), executor_, quorum_policy_);
+auto read_and_write_register_command_handler::handle() const -> std::optional<register_dto> try {
+  auto const cluster_snapshot = create_cluster_snapshot(executor_, quorum_policy_);
+  if (not cluster_snapshot.has_latest_snapshot()) {
+    return std::nullopt;
   }
 
-  return latest_snapshot;
+  make_write_phase_if_need(cluster_snapshot, executor_, quorum_policy_);
+
+  return register_dto::hydrate(cluster_snapshot.get_latest_value(), cluster_snapshot.get_latest_timestamp());
+} catch ([[maybe_unused]] cluster_exception const& what) {
+  return std::nullopt;
 }
 
 }  // namespace dsac::application::command::coordinator
