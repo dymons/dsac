ebd77153dfee1f2126647df9f870da8c17d87623
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-17 12:00:29 +0300

    Make tests for cluster dto

--- dsac/examples/dist.registry.replication/unittests/domain/test_cluster_observers.cpp
+++ dsac/examples/dist.registry.replication/unittests/domain/test_cluster_observers.cpp
@@ -37,3 +37,15 @@ TEST_CASE("Cluster is inconsistent with exceptions, get up-to-date register valu
   REQUIRE(cluster.get_latest_value() == 2);
   REQUIRE(cluster.get_latest_timestamp() == 2UL);
 }
+
+TEST_CASE("Cluster is consistent with only exceptions, do not get up-to-date register value", "[cluster][observers]") {
+  dsac::dynamic_array<dsac::result<dsac::domain::register_dto>> snapshots{
+      dsac::result<dsac::domain::register_dto>{std::make_exception_ptr(std::logic_error{""})},
+      dsac::result<dsac::domain::register_dto>{std::make_exception_ptr(std::logic_error{""})},
+      dsac::result<dsac::domain::register_dto>{std::make_exception_ptr(std::logic_error{""})}};
+  dsac::domain::cluster_dto cluster = dsac::domain::cluster_dto::hydrate(snapshots);
+
+  REQUIRE(cluster.is_consistent());
+  REQUIRE_THROWS_AS((void)cluster.get_latest_value(), dsac::domain::not_found_latest_value);
+  REQUIRE_THROWS_AS((void)cluster.get_latest_timestamp(), dsac::domain::not_found_latest_timestamp);
+}
