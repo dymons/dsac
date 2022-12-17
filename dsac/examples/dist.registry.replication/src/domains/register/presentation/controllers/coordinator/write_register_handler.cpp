93fae623e5af45b78115b72c46b9d80f6e2e32b1
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2022-12-17 13:11:34 +0300

    Refactoring code

--- dsac/examples/dist.registry.replication/src/domains/register/presentation/controllers/coordinator/write_register_handler.cpp
+++ dsac/examples/dist.registry.replication/src/domains/register/presentation/controllers/coordinator/write_register_handler.cpp
@@ -5,6 +5,22 @@
 
 #include <nlohmann/json.hpp>
 
+namespace dsac::application::command::coordinator {
+
+void from_json(const nlohmann::json& request, write_register_command& p) {
+  if (!request.contains("value") || !request["value"].is_number_integer()) [[unlikely]] {
+    throw dsac::presentation::invalid_argument{"Input data is incorrect or the required field 'value' is missing"};
+  }
+  if (!request.contains("timestamp") || !request["timestamp"].is_number_unsigned()) [[unlikely]] {
+    throw dsac::presentation::invalid_argument{"Input data is incorrect or the required field 'timestamp' is missing"};
+  }
+
+  p.value     = request["value"].get<std::int32_t>();
+  p.timestamp = request["timestamp"].get<std::size_t>();
+}
+
+}  // namespace dsac::application::command::coordinator
+
 namespace dsac::presentation::coordinator {
 
 using application::command::coordinator::write_register_command;
@@ -12,16 +28,7 @@ using application::command::coordinator::write_register_command_handler;
 using infrastructure::quorum::majority_quorum_policy;
 
 auto write_register_handler::handle(nlohmann::json const& request) -> nlohmann::json {
-  if (!request.contains("value") || !request["value"].is_number_integer()) [[unlikely]] {
-    throw invalid_argument{"Input data is incorrect or the required field 'value' is missing"};
-  }
-  if (!request.contains("timestamp") || !request["timestamp"].is_number_unsigned()) [[unlikely]] {
-    throw invalid_argument{"Input data is incorrect or the required field 'timestamp' is missing"};
-  }
-
-  write_register_command write_register_command{};
-  write_register_command.value     = request["value"].get<std::int32_t>();
-  write_register_command.timestamp = request["timestamp"].get<std::size_t>();
+  auto const write_register_command = request.get<coordinator::write_register_command>();
 
   write_register_command_handler write_register_command_handler{get_executor(), make_shared<majority_quorum_policy>()};
   write_register_command_handler.handle(write_register_command);
--- dsac/examples/dist.registry.replication/src/domains/register/presentation/controllers/replica/write_register_handler.cpp
+++ dsac/examples/dist.registry.replication/src/domains/register/presentation/controllers/replica/write_register_handler.cpp
@@ -5,6 +5,22 @@
 
 #include <nlohmann/json.hpp>
 
+namespace dsac::application::command::replica {
+
+void from_json(const nlohmann::json& request, write_register_command& p) {
+  if (!request.contains("value") || !request["value"].is_number_integer()) [[unlikely]] {
+    throw dsac::presentation::invalid_argument{"Input data is incorrect or the required field 'value' is missing"};
+  }
+  if (!request.contains("timestamp") || !request["timestamp"].is_number_unsigned()) [[unlikely]] {
+    throw dsac::presentation::invalid_argument{"Input data is incorrect or the required field 'timestamp' is missing"};
+  }
+
+  p.value     = request["value"].get<std::int32_t>();
+  p.timestamp = request["timestamp"].get<std::size_t>();
+}
+
+}  // namespace dsac::application::command::replica
+
 namespace dsac::presentation::replica {
 
 using application::command::replica::write_register_command;
@@ -12,16 +28,7 @@ using application::command::replica::write_register_command_handler;
 using infrastructure::inmemory::register_repository;
 
 auto write_register_handler::handle(nlohmann::json const& request) -> nlohmann::json {
-  if (!request.contains("value") || !request["value"].is_number_integer()) [[unlikely]] {
-    throw invalid_argument{"Input data is incorrect or the required field 'value' is missing"};
-  }
-  if (!request.contains("timestamp") || !request["timestamp"].is_number_unsigned()) [[unlikely]] {
-    throw invalid_argument{"Input data is incorrect or the required field 'timestamp' is missing"};
-  }
-
-  write_register_command write_register_command{};
-  write_register_command.value     = request["value"].get<std::int32_t>();
-  write_register_command.timestamp = request["timestamp"].get<std::size_t>();
+  auto const write_register_command = request.get<replica::write_register_command>();
 
   write_register_command_handler write_register_command_handler{make_shared<register_repository>()};
   write_register_command_handler.handle(write_register_command);
