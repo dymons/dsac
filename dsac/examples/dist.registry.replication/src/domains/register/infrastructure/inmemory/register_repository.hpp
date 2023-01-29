7be6a16ed571793f8899440c6788af9230de4e64
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2023-01-29 11:28:40 +0300

    Refactoring code

--- dsac/examples/dist.registry.replication/src/domains/register/infrastructure/inmemory/register_repository.hpp
+++ dsac/examples/dist.registry.replication/src/domains/register/infrastructure/inmemory/register_repository.hpp
@@ -6,13 +6,6 @@ namespace dsac::infrastructure::inmemory {
 
 class register_repository final : public domain::register_repository_base {
 public:
-  register_repository()                                          = default;
-  register_repository(const register_repository&)                = default;
-  register_repository(register_repository&&) noexcept            = default;
-  register_repository& operator=(const register_repository&)     = default;
-  register_repository& operator=(register_repository&&) noexcept = default;
-  ~register_repository() override                                = default;
-
   auto write(domain::register_value_object const& dto) -> void override;
   auto reset() -> void override;
   auto read() -> std::optional<domain::register_value_object> override;
