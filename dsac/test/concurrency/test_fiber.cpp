07c442e88c8ae5ccf2a2ab50ab763ab79d41af34
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2023-04-30 19:28:09 +0300

    Refactoring code

--- dsac/test/concurrency/test_fiber.cpp
+++ dsac/test/concurrency/test_fiber.cpp
@@ -1,11 +1,12 @@
 #include <catch2/catch.hpp>
 
 #include <dsac/concurrency/fibers/scheduler.hpp>
-#include <dsac/container/intrusive/list.hpp>
 
 TEST_CASE("Fiber scheduler", "[fibers]") {
+  bool executed = false;
+
   auto scheduler = dsac::fiber_scheduler::make();
-  scheduler.running_entry_routing([] -> void {
-    int a = 1;
-  });
+  scheduler.running_entry_routing([&executed] -> void { executed = true; });
+
+  REQUIRE(executed);
 }
