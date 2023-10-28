09dc2330ec4ee320944f96ed1b0871af19b68f93
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2023-10-28 17:02:12 +0300

    Try impl refinement types

--- dsac/test/functional/test_refinement_type.cpp
+++ dsac/test/functional/test_refinement_type.cpp
@@ -4,17 +4,23 @@
 
 namespace {
 
-template <std::size_t N>
+template <int N>
 struct should_be_less_then final {
-  auto operator()(std::size_t const& value) -> bool {
+  auto operator()(int const& value) -> bool {
     return value < N;
   };
 };
 
-template <std::size_t N>
+template <int N>
 struct should_be_greater_then final {
-  auto operator()(std::size_t const& value) -> bool {
+  auto operator()(int const& value) -> bool {
     return value > N;
+  };
+};
+
+struct should_be_positive final {
+  auto operator()(int const& value) -> bool {
+    return value >= 0;
   };
 };
 
@@ -22,32 +28,51 @@ struct should_be_greater_then final {
 
 TEST_CASE("Refinement type should be constructable", "[refinement_type][default]") {
   SECTION("Construct refinement type with empty refinements") {
-    using register_value        = dsac::refinement_type<std::size_t, struct RegisterValue>;
-    [[maybe_unused]] auto value = register_value{1z};
+    using register_value        = dsac::refinement_type<int, struct RegisterValue>;
+    [[maybe_unused]] auto value = register_value{1};
   }
   SECTION("Construct refinement type with some refinements") {
-    using register_value        = dsac::refinement_type<std::size_t, struct RegisterValue, should_be_less_then<256z>>;
-    [[maybe_unused]] auto value = register_value{1z};
+    using register_value        = dsac::refinement_type<int, struct RegisterValue, should_be_less_then<256>>;
+    [[maybe_unused]] auto value = register_value{1};
   }
 }
 
 TEST_CASE("Refinement type should throw by refinements", "[refinement_type][default]") {
-  // clang-format off
-  using register_value =
-      dsac::refinement_type<
-          std::size_t,
-          struct RegisterValue,
+  SECTION("should_be_greater_then && should_be_less_then") {
+    // clang-format off
+    using register_value =
+        dsac::refinement_type<
+            int,
+            struct RegisterValue,
 
-          // Refinements
-          should_be_greater_then<100z>,
-          should_be_less_then<256z>
-      >;
-  // clang-format on
+            // Refinements
+            should_be_greater_then<100z>,
+            should_be_less_then<256z>
+        >;
+    // clang-format on
 
-  SECTION("Refinement type should throw by condition should_be_less_then") {
-    REQUIRE_THROWS_AS(register_value{257z}, dsac::runtime_refinement_error);
+    SECTION("Refinement type should throw by condition should_be_less_then") {
+      REQUIRE_THROWS_AS(register_value{257}, dsac::runtime_refinement_error);
+    }
+    SECTION("Refinement type should throw by condition should_be_greater_then") {
+      REQUIRE_THROWS_AS(register_value{99}, dsac::runtime_refinement_error);
+    }
   }
-  SECTION("Refinement type should throw by condition should_be_greater_then") {
-    REQUIRE_THROWS_AS(register_value{99z}, dsac::runtime_refinement_error);
+
+  SECTION("should_be_positive") {
+    // clang-format off
+    using register_value =
+        dsac::refinement_type<
+            int,
+            struct RegisterValue,
+
+            // Refinements
+            should_be_positive
+        >;
+    // clang-format on
+
+    SECTION("Refinement type should throw by condition should_be_positive") {
+      REQUIRE_THROWS_AS(register_value{-1}, dsac::runtime_refinement_error);
+    }
   }
 }
