464b14549fe113bf35310fcc828a95956b865ac1
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2023-10-28 16:41:58 +0300

    Try impl refinement types

--- dsac/test/functional/test_refinement_type.cpp
+++ dsac/test/functional/test_refinement_type.cpp
@@ -20,3 +20,11 @@ TEST_CASE("Refinement type should be constructable", "[refinement_type][default]
     [[maybe_unused]] auto value = register_value{1z};
   }
 }
+
+TEST_CASE("Refinement type should throw by refinements", "[refinement_type][default]") {
+  using register_value = dsac::refinement_type<std::size_t, struct RegisterValue, should_be_less_then<256z>>;
+
+  SECTION("Refinement type should throw by condition should_be_less_then") {
+    REQUIRE_THROWS_AS(register_value{257z}, dsac::runtime_refinement_error);
+  }
+}
