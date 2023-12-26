c7968b6c75f344a89f056df10c0d23a1e7f56650
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2023-12-26 13:12:43 +0300

    Impl

--- dsac/include/dsac/cache/lru-k.hpp
+++ dsac/include/dsac/cache/lru-k.hpp
@@ -17,6 +17,11 @@ public:
   /*!
     \brief
         User constructor, constructs a cache with limited amount of space.
+
+    \param capacity
+        The maximum number of values the lru_k will be required to store
+    \param k
+        The maximum number of values the lru_k will be required to store by K-distance.
   */
   lru_k(std::size_t capacity, std::size_t k);
 
@@ -28,12 +33,40 @@ public:
 
   // Observers
 
+  /*!
+    \brief
+        Return LRU-K size, which tracks the number of value at the cache.
+  */
   auto size() const -> std::size_t;
 
   // Modifiers
 
+  /*!
+    \brief
+        Create a new entry for access if KV has not been seen before.
+
+        If KV is already exist, value will not be updated.
+        If KV is exist at history cache, then move KV to buffer cache with promote.
+        If KV is exist at buffer cache, then promote.
+
+    \param key
+        A new key to store the cache
+    \param value
+        A new value to store the cache
+  */
   auto put(Key key, Value value) -> void;
 
+  /*!
+    \brief
+        Find KV at cache.
+
+        If KV is not exist, return nullptr.
+        If KV is exist at history cache, then move KV to buffer cache with promote and return value.
+        If KV is exist at buffer cache, then promote and return value.
+
+     \param key
+        The key that will be used to search in the cache
+  */
   auto get(Key const& key) -> Value*;
 
   // Testsuite
